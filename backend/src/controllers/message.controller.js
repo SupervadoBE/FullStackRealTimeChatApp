e9188348214fee88271ba0075804086f6e7f2d5f
Cihan <cihansadik.bas@gmail.com>
2025-11-29 17:50:47 +0300

    Add Socket.io

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -60,7 +61,11 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    //todo : Real-time functionality goes here with socket.io
+    
+    const receiverSocketId = getReceiverSocketId(receiverId)
+    if(receiverSocketId){
+      io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
